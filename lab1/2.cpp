38a9631a5e7a6771947ca53d64bd09e220148023
Kurbatov2012 <m.kurbatov@g.nsu.ru>
2022-03-16 23:00:41 +0200

    first commit

---
+++ lab1/2.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+#include <mpi/mpi.h>
+#include <string.h>
+#include <vector>
+#include <cmath>
+#include <unistd.h>
+#define t 0.00001
+
+int get_lrows(int N, int rank, int size);
+
+void print_(double* arr, int N, int size, int rank) {
+    for(int i = 0; i < size; i++){
+        if(rank == i) {
+            std::cout << "rank = " << rank << std::endl;
+            for(int i = 0; i < get_lrows(N, rank, size); i++) {
+                for(int j = 0; j < N; j++) {
+                    std::cout << arr[i*N + j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+}
+
+void print_vector(double* arr, int N, int size, int rank) {
+    for(int i = 0; i < size; i++) {
+        if(rank == i) {
+            std::cout << "rank = " << rank << std::endl;
+            for(int j = 0; j < N; j++) {
+                std::cout << arr[j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+}
+
+struct Matrix {
+    double* matrix;
+    int s_row; // номер первой строки процесса в матрице
+    int e_row; // номер последней строки процесса в матрице
+    Matrix(int& N, int& s_row, int& e_row): s_row(s_row), e_row(e_row), matrix(new double[N * (e_row - s_row + 1)]) {}
+};
+
+void collect_vectors(double* res, double* res1, int size, Matrix& A, int N) {
+    int* arr = new int[size];
+    int* arr_size = new int[size];
+
+    for(int i = 0; i < size; i++) {
+        arr_size[i] = N;
+        arr[i] = i*N;
+    }
+
+    double* all_vectors = new double[N * size]();
+    for(int i = 0; i < A.s_row; i++) {
+        res[i] = 0;
+    }
+    for(int i = A.e_row + 1; i < N; i++) {
+        res[i] = 0;
+    }
+    //
+    MPI_Allgatherv(res, N, MPI_DOUBLE, all_vectors, arr_size, arr, MPI_DOUBLE, MPI_COMM_WORLD);
+
+    for(int i = 0; i < N; i++) {
+        res1[i] = 0; // занулим значения с прошлых шагов
+        for(int j = 0; j < size; j++) {
+            res1[i] += all_vectors[j*N + i];   
+        }
+    }
+}
+
+
+double norm(double* x, int N, int size) {
+    double sum = 0;
+    for(int i = 0; i < N; i++) {
+        sum += x[i]*x[i];
+    }
+    return std::sqrt(sum);
+}
+
+inline int index(const int& i, const int& j, const int& N) {
+    return i * N + j; 
+}
+
+double scalar_mul(double* vec_1, double* vec_2, int N) {
+    double res = 0.0;
+    for(int i = 0; i < N; i++) {
+        res += vec_1[i] * vec_2[i]; 
+    }
+    return res;
+}
+
+// Множаем матрицу на полный вектор
+void mul(double* R, Matrix& A, double* B, int N) {
+    double* tmp = new double[N]();
+
+    for(int i = 0; i < N; i++) {
+        tmp[i] = scalar_mul(A.matrix + i * N, B, N);
+    }
+
+    memcpy(R, tmp, N * sizeof(double));
+    delete[] tmp;
+}
+
+// умножим матрицу на часть вектора
+void mul(double* R, Matrix& A, double* B, int N, int rank) {
+    double* tmp = new double[N]();
+    int lines = (A.e_row - A.s_row + 1);
+    for(int i = 0; i < lines; i++) {
+        tmp[i + A.s_row] = scalar_mul(A.matrix + (i * N), B, N);
+    }
+
+    memcpy(R, tmp, N * sizeof(double));
+    delete[] tmp;
+}
+
+void sub(double* R, double* A, double* B, int N) {
+    for(int i = 0; i < N; i++) {
+        R[i] = A[i] - B[i];
+    }
+}
+
+void sub(double* R, double* A, double* B, int N, int start, int end) {
+    for(int i = start; i <= end; i++) {
+        R[i] = A[i] - B[i];
+    }
+}
+
+void print(double* x, int N, int M){
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++) {
+            int ind = index(i, j, N);
+            std::cerr << x[ind] << " "; 
+        }
+        std::cerr << std::endl; 
+    }
+    std::cerr << std::endl; 
+}
+
+int get_lrows(int N, int rank, int size){
+    int lrows = N / size;
+    int b = N % size;
+    lrows += (rank < b); 
+    return lrows;
+}
+
+Matrix create_matrix(int N, int& rank, int& size) {
+    int lrows = get_lrows(N, rank, size);
+    int s_row = 0, e_row = 0;
+    for(int i = 0; i < rank; i++) {
+        s_row += get_lrows(N, i, size);
+    }
+    e_row = s_row + lrows - 1;
+    // std::cout << "rank = " << rank << " s_row = " << s_row << " e_row = " << e_row << " lrows = " << lrows << std::endl;
+
+    Matrix m = Matrix(N, s_row, e_row);
+
+    // fill matrix
+    for(int i = 0; i < N * lrows; i++) {
+        m.matrix[i] = 1;
+    }
+    int a = 0;
+    for(int i = s_row; i <= e_row ; i++) {
+        a = i + N * (i - s_row);
+        m.matrix[a] = 2;
+    }
+    return m;
+}
+
+// size - кол-во строк у процесса
+void f(Matrix A, double* X_n, double* complete_vector, double* B, int N, int size, int rank) {
+    double* X_n_1 = new double[N]();
+    memcpy(X_n, complete_vector, N * sizeof(double));
+    memcpy(X_n_1, complete_vector, N * sizeof(double));
+
+    mul(X_n, A, X_n, N, rank);
+
+    sub(X_n, X_n, B, N, A.s_row, A.e_row);
+
+    for(int i = 0; i < N; i++) {
+        X_n[i] *= t;
+    }
+
+    sub(X_n_1, X_n_1, X_n, N, A.s_row, A.e_row);
+    memcpy(X_n, X_n_1, N * sizeof(double));
+
+    collect_vectors(X_n, complete_vector, size, A, N);
+    delete[] X_n_1;
+}
+
+double g(Matrix A, double* X, double* B, int N, int size, int rank) {
+    double* res = new double[N]();
+    double* res1 = new double[N]();
+
+    mul(res, A, X, N, rank); // умножаем матрицу на полный вектор
+
+    collect_vectors(res, res1, size, A, N); // res1 - complete vector
+
+    sub(res1, res1, B, N); // от результата отнимаем B
+    double n = norm(res1, N, size); 
+    double n1 = norm(B, N, size);
+    delete[] res;
+    delete[] res1;
+    return n / n1;
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+    int size, rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int N = std::atoi(argv[1]);  
+
+    Matrix A = create_matrix(N, rank, size);
+
+    double* B = new double[N];
+    for(int i = 0; i < N; i++) {
+        B[i] = N + 1;
+    }
+    double* X = new double[N]();
+    double* complete_vector = new double[N](); // собранный из кусочков вектор
+
+    while(g(A, complete_vector, B, N, size, rank) >=  0.00001) {
+        f(A, X, complete_vector, B, N, size, rank);
+        // sleep(1);
+    }
+
+    print_vector(complete_vector, N, size, rank);
+    delete[] B;
+    delete[] X;
+    MPI_Finalize();
+}
---
+++ lab1/4.cpp
@@ -0,0 +1,256 @@
+#include <iostream>
+#include <mpi/mpi.h>
+#include <string.h>
+#include <vector>
+#include <cmath>
+#include <unistd.h>
+#define t 0.0001
+
+// Возвращает количество строк матрицы у процесса rank
+int get_lrows(int N, int rank, int size){
+    int lrows = N / size;
+    int b = N % size;
+    lrows += (rank < b); 
+    return lrows;
+}
+
+// rank - ранг текущего процесса
+void send_piece_of_vector_next(double* piece_of_vector, double* piece_of_vector_2, int rank, int size, int N) {
+    MPI_Status status;
+    int max_len_of_piece_of_vector = (N / size) + (N % size != 0);  
+    
+    if(rank % 2 == 0) {
+        MPI_Send(piece_of_vector, max_len_of_piece_of_vector, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD);
+        MPI_Recv(piece_of_vector, max_len_of_piece_of_vector, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+    }
+    else {
+        MPI_Recv(piece_of_vector_2, max_len_of_piece_of_vector, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        MPI_Send(piece_of_vector, max_len_of_piece_of_vector, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD);
+        memcpy(piece_of_vector, piece_of_vector_2, max_len_of_piece_of_vector * sizeof(double));
+    }
+}
+
+void print_(double* arr, int N, int size, int rank) {
+    for(int i = 0; i < size; i++){
+        if(rank == i) {
+            std::cout << "rank = " << rank << std::endl;
+            for(int i = 0; i < get_lrows(N, rank, size); i++) {
+                for(int j = 0; j < N; j++) {
+                    std::cout << arr[i*N + j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+}
+
+void print_vector(double* arr, int N, int size, int rank) {
+    for(int i = 0; i < size; i++) {
+        if(rank == i) {
+            std::cout << "rank = " << rank << std::endl;
+            for(int j = 0; j < N; j++) {
+                std::cout << arr[j] << " ";
+            }
+            std::cout << std::endl;
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+}
+
+struct Matrix {
+    double* matrix;
+    int s_row; // номер первой строки процесса в матрице
+    int e_row; // номер последней строки процесса в матрице
+    Matrix(int& N, int& s_row, int& e_row): s_row(s_row), e_row(e_row), matrix(new double[N * (e_row - s_row + 1)]) {}
+};
+
+
+// возвращает номер строки матрицы, с корого начинается кусок матрицы процесса rank
+int get_number_of_fisrt_line_in_matrix(int N, int rank, int size) {
+    int sum = 0;
+    for(int i = 0; i < rank; i++) {
+        sum += get_lrows(N, i, size);
+    }
+    return sum;
+}
+
+Matrix create_matrix(int N, int& rank, int& size) {
+    int lrows = get_lrows(N, rank, size);
+    int s_row = 0, e_row = 0;
+    for(int i = 0; i < rank; i++) {
+        s_row += get_lrows(N, i, size);
+    }
+    e_row = s_row + lrows - 1;
+    // std::cout << "rank = " << rank << " s_row = " << s_row << " e_row = " << e_row << " lrows = " << lrows << std::endl;
+
+    Matrix m = Matrix(N, s_row, e_row);
+
+    // fill matrix
+    for(int i = 0; i < N * lrows; i++) {
+        m.matrix[i] = 1;
+    }
+    int a = 0;
+    for(int i = s_row; i <= e_row ; i++) {
+        a = i + N * (i - s_row);
+        m.matrix[a] = 2;
+    }
+    return m;
+}
+
+// неполное скалярное произведение
+double scalar_mul(double* vec_1, double* vec_2, int lines) {
+    double res = 0.0;
+    for(int i = 0; i < lines; i++) {
+        res += vec_1[i] * vec_2[i]; 
+    }
+    return res;
+}
+
+// N - ширина матрицы
+// number_of_lines - размер вектора X
+// rank - процесс, из которого вектор Х пришел
+void mul(double* R, Matrix& A, double* X, int N, int number_of_lines, int rank, int size) {
+    double* tmp = new double[number_of_lines]();
+    memcpy(tmp, R, number_of_lines * sizeof(double));
+
+    int start = get_number_of_fisrt_line_in_matrix(N, rank, size); // номер, с какого надо начинать читать строку в матрице
+    // std::cerr << start << std::endl;
+    int height_of_piece_of_matrix_A = A.e_row - A.s_row + 1;
+    
+    for(int i = 0; i < height_of_piece_of_matrix_A; i++) {
+        tmp[i] += scalar_mul(A.matrix + i * N + start, X, number_of_lines);
+    }
+
+    memcpy(R, tmp, number_of_lines * sizeof(double));
+    delete[] tmp;
+}
+
+void sub(double* R, double* A, double* B, int N) {
+    for(int i = 0; i < N; i++) {
+        R[i] = A[i] - B[i];
+    }
+}
+
+// size - кол-во строк у процесса
+// number_of_lines - число строк у текущего процесса
+void f(Matrix A, double* X_n, double* piece_of_vector_2, int number_of_lines, double* B, int N, int size, int rank) {
+    int current_rank = rank;                                // отражает ранг того процесса, чей кусочек вектора сейчас у данного процесса
+    int max_len_of_piece_of_vector = (N / size) + (N % size != 0);   
+    double* X_n_1 = new double[max_len_of_piece_of_vector]();
+    double* D = new double[max_len_of_piece_of_vector]();
+    
+    memcpy(X_n_1, X_n, max_len_of_piece_of_vector * sizeof(double));
+    // print_vector(X_n_1, max_len_of_piece_of_vector, size, rank);
+
+    for(int i = 0; i < size; i++) {                                             // цикл по процессам 
+        // std::cerr << "LOOP " << std::endl;
+        int lines = get_lrows(N, current_rank, size);                           // количество строк у процесса
+        
+        // print_vector(X_n, max_len_of_piece_of_vector, size, rank);
+
+        mul(D, A, X_n, N, lines, current_rank, size);                                 // умножили, что могли
+
+        // print_vector(D, max_len_of_piece_of_vector, size, rank);
+
+        send_piece_of_vector_next(X_n, piece_of_vector_2, rank, size, N);       // циклически поменяли кусочки векторов X
+        current_rank = (current_rank + 1) % size;                                       // ранг процесса с которого кусочек вектора X сейчас
+    }
+    // print_vector(D, max_len_of_piece_of_vector, size, rank);
+
+    sub(D, D, B, number_of_lines);   
+                                           // Ax - B
+    for(int i = 0; i < number_of_lines; i++) D[i] *= t;                       // t(Ax - B)
+    sub(X_n_1, X_n_1, D, number_of_lines);                                    // x - t(Ax - B)
+    memcpy(X_n, X_n_1, max_len_of_piece_of_vector * sizeof(double));                                     // результат в X
+    delete[] X_n_1;
+    delete[] D;
+}
+
+double g(Matrix A, double* X_n, double* piece_of_vector_2, double* B, int N, int size, int rank, int number_of_lines) {
+    int current_rank = rank;                 
+    int max_len_of_piece_of_vector = (N / size) + (N % size != 0);                // отражает ранг того процесса, чей кусочек вектора сейчас у данного процесса
+    double* X_n_1 = new double[max_len_of_piece_of_vector]();
+
+    memcpy(X_n_1, X_n, max_len_of_piece_of_vector * sizeof(double));
+
+    // print_vector(X_n, max_len_of_piece_of_vector, size, rank);
+    // print_vector(B, max_len_of_piece_of_vector, size, rank);
+
+    for(int i = 0; i < size; i++) {                                         // цикл по процессам 
+        int lines = get_lrows(N, current_rank, size);                           // количество строк у процесса
+        // print_vector(X_n, max_len_of_piece_of_vector, size, rank);
+        // std::cerr << " Vector recieved from " << current_rank << " proccees" << std::endl;
+        // print_vector(X_n, max_len_of_piece_of_vector, size, rank);
+
+        mul(X_n_1, A, X_n, N, lines, current_rank, size);                            // умножили, что могли
+        // print_vector(X_n_1, max_len_of_piece_of_vector, size, rank);
+        send_piece_of_vector_next(X_n, piece_of_vector_2, rank, size, N);       // циклически поменяли кусочки векторов X
+        current_rank = (current_rank + 1) % size;                            // ранг процесса с которого кусочек вектора X сейчас
+    }
+
+    // send_piece_of_vector_next(X_n, piece_of_vector_2, rank, size, N);       // циклически поменяли кусочки векторов X
+    // print_vector(X_n_1, max_len_of_piece_of_vector, size, rank);
+    sub(X_n_1, X_n_1, B, number_of_lines);   
+  
+
+    double piece_of_norm = scalar_mul(X_n_1, X_n_1, number_of_lines);
+    double full_norm = 0;
+    MPI_Allreduce(&piece_of_norm, &full_norm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    full_norm = std::sqrt(full_norm);
+    double piece_of_norm_B = scalar_mul(B, B, number_of_lines);
+    double full_norm_B = 0;
+    MPI_Allreduce(&piece_of_norm_B, &full_norm_B, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    full_norm_B = sqrt(full_norm_B);
+    delete[] X_n_1;
+    return full_norm / full_norm_B;
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+    int size, rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int N = std::atoi(argv[1]);  
+
+    Matrix A = create_matrix(N, rank, size);
+    print_(A.matrix, N, size, rank);            // +
+
+    int number_of_lines = get_lrows(N, rank, size);
+
+    double* B = new double[number_of_lines]();
+    for(int i = 0; i < number_of_lines; i++) B[i] = N + 1;
+    
+    double* copy_piece_of_vectorX = nullptr;
+
+    int max_len_of_piece_of_vector = (N / size) + (N % size != 0);                
+    double* piece_of_vectorX = new double[max_len_of_piece_of_vector](); // тк будем пересылать по кругу, то все вектора должны быть одинакого размера = макс
+
+    // for(int i = 0; i < max_len_of_piece_of_vector; i++) {
+    //     piece_of_vectorX[i] = rank;
+    // }
+
+    if(rank % 2 == 1) { // четные процессы имеют второй буфер для циклического пересыла
+        copy_piece_of_vectorX = new double[max_len_of_piece_of_vector]();
+    }
+
+    // print_vector(piece_of_vectorX, max_len_of_piece_of_vector, size, rank);
+    // std::cout << g(A, piece_of_vectorX, copy_piece_of_vectorX, B, N, size, rank, number_of_lines);
+    // std::cerr << "After G";
+    while(g(A, piece_of_vectorX, copy_piece_of_vectorX, B, N, size, rank, number_of_lines) >=  0.00001) {
+        print_vector(piece_of_vectorX, max_len_of_piece_of_vector, size, rank);
+        f(A, piece_of_vectorX, copy_piece_of_vectorX, number_of_lines, B, N, size, rank);
+        sleep(1);
+    }
+
+
+
+    print_vector(piece_of_vectorX, max_len_of_piece_of_vector, size, rank);
+    delete[] B;
+    delete[] piece_of_vectorX;
+    delete[] copy_piece_of_vectorX;
+    MPI_Finalize();
+}
+
+// Проблема что при  mpirun -n 2 ./a.out 3 выводит 0,784 0.788 0.784, а должно 0,784 0.784 0.784
