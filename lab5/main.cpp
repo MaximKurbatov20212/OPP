5a47915490c90ec03f91fdd2772b7a92552cf34a
Kurbatov2012 <m.kurbatov@g.nsu.ru>
2022-05-24 21:30:21 +0200

    lab already completed

---
+++ lab5/main.cpp
@@ -0,0 +1,197 @@
+#include <pthread.h>
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <mpi/mpi.h>
+#include <cstring>
+#include <unistd.h>
+#include <queue>
+
+#define ERROR 1
+#define SUCCESS 0 
+
+#define EXECUTOR_FINISHED_WORK -1
+
+#define SENDING_TASKS 103 
+#define SENDING_TASK_COUNT 102 
+#define GIVE_ME_TASKS 100
+
+#define HAVE_NO_TASKS -1
+#define ITERATION_ENDED -2
+#define L 100
+
+int number_of_tasks = 100;
+int number_of_lists = 100;
+
+int rank = 0;
+int size = 0;
+
+
+std::queue<long long> tasks;
+
+int global_iteration = 0;
+double local_result = 0;
+double global_result = 0;
+int is_end = false;
+
+pthread_mutex_t mutex;
+pthread_mutexattr_t mutex_attr;
+
+
+double doing(int repeat_num) {
+    int result = 0;
+    for(int i = 0; i < repeat_num; i++) {
+        result += pow(sin((double) i) + cos((double) i), i);
+    }
+    return result;
+}
+
+void get_tasks(int iteration) {
+    pthread_mutex_lock(&mutex);
+    for(int i = 0; i < number_of_tasks; i++) {
+        tasks.push(abs(50 - i % number_of_lists) * abs(rank - (iteration % size)) * L);
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+void execute_tasks() {
+    while(true) {
+        pthread_mutex_lock(&mutex);
+        if(tasks.empty()) {
+            pthread_mutex_unlock(&mutex);
+            return;
+        }
+
+        local_result += doing(tasks.front());
+        tasks.pop();
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int get_additional_tasks() {
+    int new_task = HAVE_NO_TASKS;
+
+    for(int i = 0; i < size; i++) {
+        if(rank == i) continue;
+
+        MPI_Send(&rank, 1, MPI_INT, i, GIVE_ME_TASKS, MPI_COMM_WORLD);
+
+        MPI_Recv(&new_task, 1, MPI_INT, i, SENDING_TASK_COUNT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        if(new_task == HAVE_NO_TASKS) continue;
+
+        return new_task;
+    }
+    return HAVE_NO_TASKS;
+}
+
+void stop_reciever() {
+    is_end = true;
+    int end = EXECUTOR_FINISHED_WORK;
+    MPI_Send(&end, 1, MPI_INT, rank, GIVE_ME_TASKS, MPI_COMM_WORLD);
+    printf("Job completed successfully\n");
+}
+
+void* execution(void* a) {
+    for(int i = 0; i < number_of_lists; i++) {
+        // std::cout << rank << std::endl;
+
+        // if(rank == 0) printf("\nIteration: %d\n", i);
+
+        get_tasks(i);
+        execute_tasks();
+
+        printf("[%d] My tasks were completed \n", rank);
+
+        while(true) {
+            int new_task = 0;
+
+            new_task = get_additional_tasks();
+            if(new_task == HAVE_NO_TASKS) break;
+
+            printf("[%d] Got additional task %d \n", rank, new_task);
+
+            pthread_mutex_lock(&mutex);
+            tasks.push(new_task);
+            pthread_mutex_unlock(&mutex);
+
+            execute_tasks();
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        // sleep(5);
+        global_iteration++;
+    }
+
+    stop_reciever();
+    MPI_Allreduce(&local_result, &global_result, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    pthread_exit(nullptr);
+}
+
+int receive_request() {
+    int request = 0;
+    MPI_Recv(&request, 1, MPI_INT, MPI_ANY_SOURCE, GIVE_ME_TASKS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    return request;
+}
+
+bool has_free_tasks() {
+    return !(tasks.empty());
+}
+
+void* receive_msg(void* a) {
+    while(!is_end) {
+        int id = receive_request();
+        if(id == EXECUTOR_FINISHED_WORK) break;
+
+        pthread_mutex_lock(&mutex);                
+        if(has_free_tasks()) {
+            printf("[%d] Send task to %d \n", rank, id);
+            MPI_Send(&tasks.front(), 1, MPI_INT, id, SENDING_TASK_COUNT, MPI_COMM_WORLD);
+            tasks.pop();
+        }
+        else {
+            printf("[%d] I have no tasks \n", rank);
+            int response = HAVE_NO_TASKS;
+            MPI_Send(&response, 1, MPI_INT, id, SENDING_TASK_COUNT, MPI_COMM_WORLD);
+        }
+        pthread_mutex_unlock(&mutex);                
+    }
+    pthread_exit(nullptr);
+}
+
+void main_thread_wait(pthread_t* threads) {
+    pthread_join(threads[0], nullptr);
+    pthread_join(threads[1], nullptr);
+}
+
+int main(int argc, char** argv) {
+
+    int mpi_init_tread_result;
+
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &mpi_init_tread_result);
+
+    if(mpi_init_tread_result != MPI_THREAD_MULTIPLE) {
+        MPI_Finalize();
+        return ERROR;
+    }
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    pthread_mutex_init(&mutex, &mutex_attr);
+
+    pthread_t threads[2];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    
+    pthread_create(&threads[0], &attr, execution, nullptr);
+    pthread_create(&threads[1], &attr, receive_msg, nullptr);
+
+    main_thread_wait(threads);
+
+    pthread_attr_destroy(&attr);
+    pthread_mutex_destroy(&mutex);
+    
+    MPI_Finalize();
+    return SUCCESS;
+}
